486d44b6138533444f02b4da1d04a3cdcc790dbd
Dominic Canare <dom@greenlightgo.org>
2019-08-07 20:46:00 -0700

    OS integration (system tray, hotkey)

--- LineReader/__main__.py
+++ LineReader/__main__.py
@@ -1,18 +1,69 @@
+import pkg_resources
+
 from PySide2 import QtCore, QtGui, QtWidgets
+
+from system_hotkey import SystemHotkey
+
+def getAssetPath(resource):
+	return pkg_resources.resource_filename(__name__, 'assets/' + resource)
+
+class HotkeyManager(QtCore.QObject):
+	triggered = QtCore.Signal(object)
+
+	def __init__(self, parent=None):
+		super().__init__(parent)
+		self.keys = []
+		self.hk = SystemHotkey()
+
+	def addKey(self, keyCombo):
+		self.hk.register(keyCombo, callback=self.triggered.emit)
+
 
 class LineWindow(QtWidgets.QWidget):
 	def __init__(self, parent=None):
 		super().__init__(parent)
-
 		self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
 		self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowTransparentForInput | QtCore.Qt.X11BypassWindowManagerHint)
 		self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
 		self.mouseLocation = None
 
+		self.buildTrayIcon()
+
+		self.hotkeyManager = HotkeyManager()
+		self.hotkeyManager.addKey(('super', 'shift', 'l'))
+		self.hotkeyManager.triggered.connect(self.onHotkeyTriggered)
+
 		self.timer = QtCore.QTimer()
 		self.timer.setInterval(16)
 		self.timer.timeout.connect(self._poll)
+
+
+	def buildTrayIcon(self):
+		self.trayIcon = QtWidgets.QSystemTrayIcon(QtGui.QIcon(getAssetPath('icon.png')), self)
+		self.trayIcon.show()
+		self.trayIcon.setToolTip('Line Reader')
+		self.trayIcon.activated.connect(self.onTrayIconActivated)
+
+		self.trayMenu = QtWidgets.QMenu('👓 Line Reader')
+		self.trayMenu.addAction('Toggle')
+		self.trayMenu.addAction('Exit')
+
+		self.trayMenu.triggered.connect(self.onActionTriggered)
+
+		self.trayIcon.setContextMenu(self.trayMenu)
+
+	def onHotkeyTriggered(self):
+		self.toggle()
+
+	def onTrayIconActivated(self):
+		self.toggle()
+
+	def onActionTriggered(self, action):
+		if action.text() == 'Exit':
+			QtWidgets.QApplication.instance().exit()
+		elif action.text() == 'Toggle':
+			self.toggle()
 
 	def enable(self):
 		self.timer.start()
@@ -23,6 +74,12 @@ class LineWindow(QtWidgets.QWidget):
 		self.timer.stop()
 		self.hide()
 
+	def toggle(self):
+		if self.isVisible():
+			self.disable()
+		else:
+			self.enable()
+
 	def _poll(self):
 		self.mouseLocation = QtGui.QCursor.pos() - self.pos()
 		self.update()
@@ -31,7 +88,7 @@ class LineWindow(QtWidgets.QWidget):
 		super().paintEvent(event)
 		if self.mouseLocation is not None:
 			painter = QtGui.QPainter(self)
-			painter.fillRect(0, self.mouseLocation.y()+10, self.width(), 2, QtCore.Qt.red)
+			painter.fillRect(0, self.mouseLocation.y()+10, self.width(), 2, QtGui.QColor(255, 128, 0, 96))
 
 app = QtWidgets.QApplication()
 window = LineWindow()
